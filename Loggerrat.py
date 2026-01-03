9385d80700226be9d16b524451c1286c632dceb3
sg-stack <147706047+sg-stack@users.noreply.github.com>
2026-01-03 14:36:48 +0530

    Edit comment in Loggerrat.py
    Updated comment in Loggerrat.py for clarity.

--- Loggerrat.py
+++ Loggerrat.py
@@ -1,5 +1,5 @@
 "THIS IS A LEETCODE PROBLEM LOGGER RATE DO NOT FORGET"
-
+"Good thing to edit this message out very quickly"
 class LoggerRateLimit:
     def __init__(self):
         self.limiter={}
